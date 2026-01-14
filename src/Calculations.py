f6f7e6e9d97f2f8c464ca0278a11a89d12a95d5e
darilsobze <darilsobze13@icloud.com>
2026-01-14 10:23:51 +0100

    fixed bugs NAN

--- src/Calculations.py
+++ src/Calculations.py
@@ -7,8 +7,8 @@ def diffusion_coefficient(capillary_radius,standard_deviation : float,t_R:float)
 
     radius_in_meter = capillary_radius * (10 ** -6)
 
-    if standard_deviation <= 0:
-        return np.nan
+    #if standard_deviation <= 0:
+      #  return np.nan
     return((radius_in_meter**2)*t_R)/(24*(standard_deviation**2))
 
 def hydrodynamic_radius(temp:float,viscosity:float,diffusion_coefficient:float):
@@ -21,13 +21,13 @@ def peclet(R_h,temperature,viscosity,capillary_radius,flow_rate):
     radius_in_meter = capillary_radius * (10 ** -6)
     Q_in_meter_cube_per_sec = (flow_rate * (10 ** -9)) / 60
 
-    if boltzmann_c == 0 or radius_in_meter <= 0 or T_in_kelvin <= 0:
-        return np.nan
+    ##if boltzmann_c == 0 or radius_in_meter <= 0 or T_in_kelvin <= 0:
+      #  return np.nan
     return (6 * viscosity * Q_in_meter_cube_per_sec * R_h) / (boltzmann_c * T_in_kelvin * radius_in_meter)
 
 def tau(T,L,viscosity,Q,R_h):
-    if viscosity <= 0 or Q <= 0 or R_h <= 0 :
-        return np.nan
+    #if viscosity <= 0 or Q <= 0 or R_h <= 0 :
+     #   return np.nan
 
     T_in_kelvin = T + 273.15
     L_in_meter = L * (10**-2)
